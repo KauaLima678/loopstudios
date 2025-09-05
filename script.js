9352a78ebcd0c7f5d6a588806fc73d36ae4debc7
KauaLima678 <kaua26kklima@gmail.com>
2025-09-05 11:54:59 -0300

    add responsivity

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const bar = document.getElementById('barButton');
+const close = document.getElementById('close');
 
 function openSide(){
-    bar.style.color = "red";
+    document.getElementById('sideBar').style.display = "block";
+}
+
+function closeSide(){
+    document.getElementById('sideBar').style.display = "none";
 }
